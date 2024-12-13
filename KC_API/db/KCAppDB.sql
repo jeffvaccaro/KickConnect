0909930d4affe4f075f52b18c9c1abf02c9dc215
Jeff V <jeff.vaccaro@gmail.com>
2024-12-13 11:24:42 -0700

    updated sql script

--- KC_API/db/KCAppDB.sql
+++ KC_API/db/KCAppDB.sql
@@ -24,6 +24,8 @@
 --
 -- Current Database: `admin`
 --
+DROP DATABASE `admin`;
+DROP DATABASE `common`;
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `admin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
