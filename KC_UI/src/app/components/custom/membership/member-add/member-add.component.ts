fa0d229409038ce25d55bd578fc786893a9d5dba
Jeff V <jeff.vaccaro@gmail.com>
2025-08-15 20:43:18 -0600

    added components for membership and plan

---
+++ KC_UI/src/app/components/custom/membership/member-add/member-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-add',
+  imports: [],
+  templateUrl: './member-add.component.html',
+  styleUrl: './member-add.component.scss'
+})
+export class MemberAddComponent {
+
+}
---
+++ KC_UI/src/app/components/custom/membership/plan-add/plan-add.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlanAddComponent } from './plan-add.component';
+
+describe('PlanAddComponent', () => {
+  let component: PlanAddComponent;
+  let fixture: ComponentFixture<PlanAddComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PlanAddComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PlanAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
